d8cadee9b7a4c7001504479540e6899c78aefd93
GAURVIT07 <dangayachgaurvit15@gmail.com>
2026-04-01 21:40:44 +0530

    W1_2

---
+++ SwapNumbers.java
@@ -0,0 +1,37 @@
+// //Q1
+// import java.util.Scanner;
+// public class SimpleInterest {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         float principal = sc.nextFloat();
+//         float rate = sc.nextFloat();
+//         float time = sc.nextFloat();
+
+//         float simpleInterest = (principal * rate * time) / 100;
+
+//         System.out.println("The Simple Interest is " + simpleInterest + 
+//             " for Principal " + principal + ", Rate of Interest " + rate + 
+//             " and Time " + time);
+//             sc.close();
+//     }
+// }
+
+
+
+
+// //Q2
+// import java.util.Scanner;
+// public class SwapNumbers {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int number1 = sc.nextInt();
+//         int number2 = sc.nextInt();
+
+//         int temp = number1;
+//         number1 = number2;
+//         number2 = temp;
+
+//         System.out.println("The swapped numbers are " + number1 + " and " + number2);
+//         sc.close();
+//     }
+// }
